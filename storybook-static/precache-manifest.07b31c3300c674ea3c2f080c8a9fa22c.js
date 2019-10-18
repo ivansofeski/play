1694d4f316a720f8be8e276812670949628df742
Ivan Sofeski <Ivan.Sofeski@cybercom.com>
2019-10-18 10:41:50 +0200

    storybook

---
+++ storybook-static/precache-manifest.07b31c3300c674ea3c2f080c8a9fa22c.js
@@ -0,0 +1,15 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "23bb3ba278bde40c85f7be5de121a93e",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.607a33fb624e36bb0e31.bundle.js"
+  },
+  {
+    "url": "runtime~main.607a33fb624e36bb0e31.bundle.js"
+  },
+  {
+    "url": "vendors~main.607a33fb624e36bb0e31.bundle.js"
+  }
+]);
